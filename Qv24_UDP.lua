17c4a652eb4759817f85f2148db618e293670ce1
John Yaldwyn <jy@xtra.co.nz>
2016-09-18 21:06:32 +1200

    Add decode of basic Infrastructure Control Words

--- Qv24_UDP.lua
+++ Qv24_UDP.lua
@@ -76,7 +76,7 @@ pkt.cols.protocol = p_QV24.name
 		elseif buf(2,1):uint() == 0x04 then subtree:append_text(" RT/RT disbaled")
 		end
 		if buf(4,1):uint() == 0x0b then subtree:append_text(", Voice")
-		elseif buf(3,1):uint() == 0x0f then subtree:append_text(", Page")
+		elseif buf(4,1):uint() == 0x0f then subtree:append_text(", Page")
 		end
 	end
 	if frame == 0x60 then
@@ -87,10 +87,13 @@ pkt.cols.protocol = p_QV24.name
 		elseif buf(2,1):uint() == 0x04 then subtree:append_text(", RT/RT disbaled")
 		end
 		if buf(4,1):uint() == 0x0b then subtree:append_text(", Voice")
-		elseif buf(3,1):uint() == 0x0f then subtree:append_text(", Page")
+		elseif buf(4,1):uint() == 0x0f then subtree:append_text(", Page")
 		end
 	end
 	if frame == 0x62 then 
+		if buf(5,1):uint() == 0x00 then subtree:append_text(" DIU 3000,")
+		elseif buf(5,1):uint() == 0x1b then subtree:append_text(" Quantar,")
+		end
 		subtree:append_text(" LDU1 RSSI= ".. buf(6,1):uint())
 		subtree:append_text(", inverse signal= ".. buf(8,1):uint())
 		subtree:append_text(", candidate adjusted MM= $".. buf(9,1))
@@ -126,6 +129,9 @@ pkt.cols.protocol = p_QV24.name
 		subtree:append_text(" last byte= $".. buf(15,1))
 	end
 	if frame == 0x6b then 
+		if buf(5,1):uint() == 0x00 then subtree:append_text(" DIU 3000,")
+		elseif buf(5,1):uint() == 0x1b then subtree:append_text(" Quantar,")
+		end
 		subtree:append_text(" LDU2 RSSI= ".. buf(6,1):uint())
 		subtree:append_text(", inverse signal= ".. buf(8,1):uint())
 		subtree:append_text(", candidate adjusted MM= $".. buf(9,1))
